e16e51fcfef84acc9839970844c8020685cb84e2
ted <82011813+unnotedme@users.noreply.github.com>
2023-02-24 01:19:05 -0400

    first commit

---
+++ src/test/java/dev/unnotedme/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.unnotedme.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
